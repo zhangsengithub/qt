623ff312c9a8a063dab21475e3ae9106a4429ad1
git <git@qq.com>
2025-07-28 21:11:20 +0800

    privoxy

--- proxy.sh
+++ proxy.sh
@@ -1,15 +1,15 @@
 function proxy_on() {
-    #export http_proxy=http://127.0.0.1:1086
-    #export https_proxy=$http_proxy
-    export ALL_PROXY="socks5://127.0.0.1:1086"
-    export http_proxy="http://127.0.0.1:1087"
-    export https_proxy="https://127.0.0.1:1087"
-    echo -e "终端代理已开启。"
-    nc -zv -X 5 -x 127.0.0.1:1086 www.google.com 443
+export ALL_PROXY="socks5://127.0.0.1:1086"
+export http_proxy="http://127.0.0.1:1087"
+export https_proxy="https://127.0.0.1:1087"
+echo -e "终端代理已开启。"
+nc -zv -X 5 -x 127.0.0.1:1086 www.google.com 443
 }
 
 function proxy_off(){
     unset http_proxy https_proxy
     echo -e "终端代理已关闭。"
 }
-proxy_on
+ps -ef|grep privoxy
+export http_proxy="http://127.0.0.1:1087"
+export https_proxy="https://127.0.0.1:1087"
